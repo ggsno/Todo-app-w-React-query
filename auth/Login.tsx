fdce373ed99d63343ef16f657081a3421c354da1
ks <ogs0518@gmail.com>
2022-08-01 20:58:15 +0900

    feat: validating email, pw in login

--- auth/Login.tsx
+++ auth/Login.tsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
+import useInput from "../hooks/useInput";
+
 const Login = () => {
-  const handleSubmit = async e => {
+  const [valid, setValid] = useState(false);
+  const email = useInput("");
+  const password = useInput("");
+
+  const validCheck = (email, passwrd) => {
+    return /@/.test(email) && /\./.test(email) && 8 <= passwrd.length;
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     const [email, password] = [e.target[0].value, e.target[1].value];
+
+    if (!validCheck(email, password)) {
+      alert("잘못된 양식");
+      return;
+    }
+
     const response = await fetch("http://localhost:8080/users/login", {
       method: "POST",
       headers: {
         Accept: "application/json",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password }),
     });
 
     if (!response.ok) {
@@ -23,15 +41,19 @@ const Login = () => {
     // 리다이렉팅
   };
 
+  useEffect(() => setValid(validCheck(email.value, password.value)));
+
   return (
     <>
       <h1>로그인</h1>
       <form name="login" onSubmit={handleSubmit}>
         <label htmlFor="email">이메일</label>
-        <input type="email" placeholder="이메일 입력" required />
+        <input type="email" placeholder="이메일 입력" {...email} required />
         <label htmlFor="password">비밀번호</label>
-        <input type="password" placeholder="8자 이상" required />
-        <button type="submit">로그인</button>
+        <input type="password" placeholder="8자 이상" {...password} required />
+        <button type="submit" disabled={!valid}>
+          로그인
+        </button>
       </form>
     </>
   );
