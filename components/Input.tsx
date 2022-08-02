a224cb1dbf75138605f68282a8ad7a9ed4bb95be
ggsno <ogs0518@gmail.com>
2022-08-02 16:16:04 +0900

    refactor: craete input component

---
+++ components/Input.tsx
@@ -0,0 +1,28 @@
+import { ChangeEvent } from "react";
+
+interface inputType {
+  type: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  required: boolean;
+  id?: string;
+  placeholder?: string;
+  labelName?: string;
+}
+
+const Input = (inputProps: inputType) => {
+  const {
+    id = inputProps.type,
+    placeholder = inputProps.type,
+    labelName = inputProps.placeholder || inputProps.type,
+    ...props
+  } = inputProps;
+  return (
+    <>
+      <label htmlFor={id}>{labelName}</label>
+      <input id={id} placeholder={placeholder} {...props} />
+    </>
+  );
+};
+
+export default Input;
