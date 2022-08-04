15de05cc6726593e29ff8dd1ee8ae890303d1317
ggsno <ogs0518@gmail.com>
2022-08-04 18:35:05 +0900

    refactor: seperate components

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -125,26 +125,7 @@ const Todo = () => {
             ))
           )}
         </S.Wrapper>
-        <S.Wrapper>
-          <h2>Todo Details</h2>
-          {!selectedTodo ? (
-            <p>Click Todo To See Details</p>
-          ) : (
-            <>
-              <h3>title</h3>
-              <p>{selectedTodo.title}</p>
-              <h3>content</h3>
-              <p>{selectedTodo.content}</p>
-              <h3>created at</h3>
-              <p>{selectedTodo.createdAt}</p>
-              <h3>updated at</h3>
-              <p>{selectedTodo.updatedAt}</p>
-              <button onClick={handleEdit} value={selectedTodo.id}>
-                edit
-              </button>
-            </>
-          )}
-        </S.Wrapper>
+
         <S.Wrapper>
           <h2>Add Todo</h2>
           <Input id="newTodoTitle" placeholder="title" {...newTodoTitle} />
