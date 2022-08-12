5ee513375d5bf7084decaae9d3b1195bd3f8a028
ggsno <ogs0518@gmail.com>
2022-08-12 10:03:20 +0900

    refactor: 안쓰는 type 삭제

--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -10,21 +10,3 @@ export interface User {
 export type UserInput = Pick<User, "email" | "password">;
 
 export type AuthInput = UserInput & { passwordCheck?: string };
-
-export type AuthContextType = {
-  valid: boolean;
-  handleLogin: (e: FormEvent<HTMLFormElement>) => void;
-  handleSignup: (e: FormEvent<HTMLFormElement>) => void;
-  inputEmail: {
-    value: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  };
-  inputPassword: {
-    value: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  };
-  inputPasswordCheck: {
-    value: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  };
-};
